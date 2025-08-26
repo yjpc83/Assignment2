97cd04b7a9aac6a7bf8d562cebbdf17b7e055643
Yu-Jen Cheng <21259802@students.ltu.edu.au>
2025-08-26 21:09:42 +1000

    added UI for displaying contact address

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -36,7 +36,17 @@ function Contact(props) {
     return (
         <div key={contact.id} className='contact' onClick={(e) => setExpanded(!expanded)}>
             <div className='title'>
-                <h3>{contact.name}</h3>
+                {/* <h3>{contact.name}</h3> */}
+                {/* <button className='button red' onClick={doDelete}>Delete Contact</button> */}
+            </div>
+
+            <div className='details'>
+                <strong>Name: </strong> {contact.name} <br />
+                <strong>Address: </strong> {contact.address} <br />
+                
+            </div>
+
+            <div>
                 <button className='button red' onClick={doDelete}>Delete Contact</button>
             </div>
 
